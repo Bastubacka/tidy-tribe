b442087e457b4009e0b79c82c19509333860336e
Bastubacka <benjamin.colas@gmail.com>
2015-11-05 17:45:00 +0000

    lucky number

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -19,5 +19,27 @@ class LuckyController extends Controller
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
     }
+    /**
+     * @Route("/")
+     */
+    public function Page1()
+    {
+        $number = rand(0, 100);
+
+        return new Response(
+            '<html><body>Page 1</body></html>'
+        );
+    }
+        /**
+     * @Route("/web")
+     */
+    public function Page2()
+    {
+        $number = rand(0, 100);
+
+        return new Response(
+            '<html><body>Page 2</body></html>'
+        );
+    }
 }
 
